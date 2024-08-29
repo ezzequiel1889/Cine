d86cedcdd3a9b53663b6c74e378d48d346e78884
Ezequiel Maidana <ezemaidana1889@gmail.com>
2024-08-29 14:20:33 -0300

    correcion

--- js/javascript.js
+++ js/javascript.js
@@ -52,50 +52,43 @@ ENVIAR.addEventListener("click", function(){
           
             
         /// PELICULA ///
-                event.preventDefault();
-                let peliculaElegida = PELICULA.value;
-        
-                mostrarPelicula.textContent = `PELICULA: ${peliculaElegida}`;
-                localStorage.setItem("PELICULA", peliculaElegida); // "CLAVE" : valor
+            let peliculaElegida = PELICULA.value;
+    
+            mostrarPelicula.textContent = `PELICULA: ${peliculaElegida}`;
+            localStorage.setItem("PELICULA", peliculaElegida); // "CLAVE" : valor
         
         
         /// DIMENSION ///
-                let dimensionValor = DIMENSION.value;
-        
-                if (dimensionValor == "3D" || dimensionValor == "4D"){
-                    mostrarDimension.textContent = `DIMENSION: ${dimensionValor}`;
-                }
-                localStorage.setItem("DIMENSION", dimensionValor); // "CLAVE" : valor
+            let dimensionValor = DIMENSION.value;
+    
+            if (dimensionValor == "3D" || dimensionValor == "4D"){
+                mostrarDimension.textContent = `DIMENSION: ${dimensionValor}`;
+            }
+            localStorage.setItem("DIMENSION", dimensionValor); // "CLAVE" : valor
         
         
         /// COMIDA ///
-                let comidaValor = txtArea.value;
-                mostrarComida.textContent = `COMIDA: ${comidaValor}`;
-                localStorage.setItem("COMIDA", comidaValor); // "CLAVE" : valor
-
-
-
-                let infoObj = {
-                    dni: dniValor,
-                    edad: edadValor,
-                    pelicula: peliculaElegida,
-                    dimension: dimensionValor,
-                    comida: comidaValor
-                }
-
-                /* infoObj es tomado como un OBJETO, 
-                 para que el localStorage lo pueda leer 
-                 en string hay que aplicar el JSON.stringify()
-                 */
-                localStorage.setItem("todaInformacion", JSON.stringify(infoObj));
-                // ahora pasa a ser un string para el localStorage
-
-                // Ya pasado a string, se puede ver en consola el obj
-                console.log(infoObj);
-
-                
-                /* funcion de dni y edad */
-                datosUsuario(dniValor,edadValor);
+            let comidaValor = txtArea.value;
+            mostrarComida.textContent = `COMIDA: ${comidaValor}`;
+            localStorage.setItem("COMIDA", comidaValor); // "CLAVE" : valor
+            let infoObj = {
+                dni: dniValor,
+                edad: edadValor,
+                pelicula: peliculaElegida,
+                dimension: dimensionValor,
+                comida: comidaValor
+            }
+            /* infoObj es tomado como un OBJETO, 
+             para que el localStorage lo pueda leer 
+             en string hay que aplicar el JSON.stringify()
+             */
+            localStorage.setItem("todaInformacion", JSON.stringify(infoObj));
+            // ahora pasa a ser un string para el localStorage
+            // Ya pasado a string, se puede ver en consola el obj
+            console.log(infoObj);
+            
+            /* funcion de dni y edad */
+            datosUsuario(dniValor,edadValor);
     })  
 
 
